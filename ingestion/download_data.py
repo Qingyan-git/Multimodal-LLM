c200743b9fbdfd340bfb8c445f66d5a39b07a9b3
Chu Qingyan <chuqingyan123@gmail.com>
2026-03-10 16:17:28 +0800

    improved code etiquette for downloading data

--- ingestion/download_data.py
+++ ingestion/download_data.py
@@ -2,23 +2,42 @@ import os
 import dotenv
 import gdown
 
+
+def download_folder(url,output_path):
+
+    '''
+    downloads a folder from google drive with the url argument and downloads into the output_path 
+    argument
+    '''
+
+
+    try:
+
+        old_files = len(os.listdir(output_path))
+
+        gdown.download_folder(
+            url = url,
+            output = output_path,
+            quiet = True,
+            use_cookies = True
+        )
+
+        new_files = len(os.listdir(output_path))
+        uploaded_files = new_files-old_files
+
+        if os.path.exists(output_path) and os.listdir(output_path):
+            print(f'Download successful. {uploaded_files} files uploaded.') 
+        else:
+            raise FileNotFoundError('Please double check the path to the dataset dump folder')
+
+    except Exception as e:
+        print(f'An error occured : {e}')
+
+    return
+
+
 dotenv.load_dotenv()
 dataset_url = os.getenv('dataset_path')
 dataset_output = os.getenv('dataset_download_path')
 
-
-try:
-    gdown.download_folder(
-        url = dataset_url,
-        output = dataset_output,
-        quiet = True,
-        use_cookies = True
-    )
-
-    if os.path.exists(dataset_output) and os.listdir(dataset_output):
-        print(f'Download successful. {len(os.listdir(dataset_output))} files uploaded.') 
-    else:
-        raise Exception('Please double check the path to the dataset dump folder')
-
-except Exception as e:
-    print(f'An error occured : {e}')
+download_folder(dataset_url,dataset_output)
