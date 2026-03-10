1c3dbd8022365059a2d2bb0e95e101b2942e5252
Chu Qingyan <chuqingyan123@gmail.com>
2026-03-10 17:13:39 +0800

    added cleanup functionality for text processing

--- ingestion/download_data.py
+++ ingestion/download_data.py
@@ -6,38 +6,34 @@ import gdown
 def download_folder(url,output_path):
 
     '''
-    downloads a folder from google drive with the url argument and downloads into the output_path 
+    Downloads a folder from google drive with the url argument and downloads into the output_path 
     argument
     '''
 
-    try:
 
-        old_files = len(os.listdir(output_path))
+    old_files = len(os.listdir(output_path))
 
-        gdown.download_folder(
-            url = url,
-            output = output_path,
-            quiet = True,
-            use_cookies = True
-        )
+    gdown.download_folder(
+        url = url,
+        output = output_path,
+        quiet = True,
+        use_cookies = True
+    )
 
-        new_files = len(os.listdir(output_path))
-        uploaded_files = new_files-old_files
+    new_files = len(os.listdir(output_path))
+    uploaded_files = new_files-old_files
 
-        if os.path.exists(output_path) and os.listdir(output_path):
-            print(f'Download successful. {uploaded_files} files uploaded.') 
-        else:
-            raise FileNotFoundError('Please double check the path to the dataset dump folder')
+    if os.path.exists(output_path) and os.listdir(output_path):
+        print(f'Download successful. {uploaded_files} files downloaded.\n')
+    else:
+        raise FileNotFoundError('Please double check the path to the dataset dump folder')
 
-    except Exception as e:
-        print(f'An error occured : {e}')
 
-    return
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    dotenv.load_dotenv()
-    dataset_url = os.getenv('dataset_path')
-    dataset_output = os.getenv('dataset_download_path')
+dotenv.load_dotenv()
+dataset_url = os.getenv('dataset_path')
+dataset_output = os.getenv('dataset_download_path')
 
-    download_folder(dataset_url,dataset_output)
+download_folder(dataset_url,dataset_output)
---
+++ processing/process_pdf.py
@@ -0,0 +1,51 @@
+import pymupdf
+import os
+import datetime
+import dotenv
+import re
+import unicodedata
+
+def clean_pdf_text(page):
+
+    '''
+    Cleans a pdf page's text content
+    '''
+
+    def unicode_normalization(page):
+        return unicodedata.normalize('NFKC',page)
+
+    def remove_unprintable_chars(page):
+        return ''.join(char for char in page if char.isprintable())
+
+    page = re.sub(r'\\s+', ' ', page)
+    page = unicode_normalization(page)
+    page = remove_unprintable_chars(page)
+
+    return page
+
+def read_doc_text(doc_path, output_path):
+
+    '''
+    Reads a pdf for its textual content only
+    '''
+
+    doc = pymupdf.open(doc_path)
+    doc_title = doc.metadata.get('title', f'No title found {datetime.datetime.now()}')
+
+    write_path = os.path.join(output_path, f'{doc_title}.txt')
+    with open(write_path, 'w', encoding='utf-8') as f:
+        for page in doc:
+            text = page.get_text()
+            cleaned_text = clean_pdf_text(text)
+            f.write(cleaned_text)
+
+    print(f'{doc_title} read and output saved.\n')
+
+
+
+# if __name__ == '__main__':
+dotenv.load_dotenv()
+test_file_path = os.getenv('test_file_path')
+test_file_output = os.getenv('test_file_output')
+
+read_doc_text(test_file_path,test_file_output)
