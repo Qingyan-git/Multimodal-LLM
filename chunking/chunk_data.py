8dd2d65d02f8a65fdeb6651f7aa6248dec1dfb62
Chu Qingyan <chuqingyan123@gmail.com>
2026-03-11 17:11:06 +0800

    need different python version

--- chunking/chunk_data.py
+++ chunking/chunk_data.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 import dotenv
 import json
-import spacy
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def retrieve_data(folder_path):
@@ -41,7 +41,7 @@ def retrieve_data(folder_path):
 
 
 
-def chunk_text(document_information, window_size=300, step_size=60):
+def chunk_text(document_information, chunk_size=300, chunk_overlap=60):
     '''
     Takes in some document_information, chunks the page_content of that document, and returns an 
     object with the chunks labelled with page number and metadata
@@ -51,10 +51,21 @@ def chunk_text(document_information, window_size=300, step_size=60):
     pointer = 0
 
     full_text,document_page_offset,doc_metadata = document_information
-    splitter = NLTKTextSplitter(separator="", language="english")  # sentence-level splitting
-    sentences = splitter.split_text(full_text)
 
-    print(sentences)
+    splitter = RecursiveCharacterTextSplitter(
+        separators=['.'],
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        keep_separator=True,
+        add_start_index=True,
+        strip_whitespace=True
+    )
+
+    chunks_with_index = splitter.split_text(full_text)
+    chunked_data = []
+
+    print(chunks_with_index)
+    
 
 
 
@@ -72,7 +83,6 @@ def chunk_text(document_information, window_size=300, step_size=60):
 
 
 #if __name__ == '__main__':
-nltk.download('punkt_tab')
 dotenv.load_dotenv()
 chunking_data_download = Path(os.getenv('chunking_data_download'))
 
